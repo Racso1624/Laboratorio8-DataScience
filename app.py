97200dc73bca5e47303b193bfde2dac4316865a0
Racso1624 <fer10_lb@outlook.com>
2023-10-22 23:12:30 -0600

    Se agregan graficos

--- app.py
+++ app.py
@@ -5,10 +5,14 @@
 
 # Importar libreria
 import streamlit as st
+import matplotlib.pyplot as plt
 from model import *
 
 # Se carga el modelo de prediccion
 predictive_model = PredictiveModel()
+
+# Para los graficos
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Titulo de la pagina
 st.title('Prediccion Precios de Alquiler')
@@ -33,3 +37,32 @@ predict_button = st.button("Calcular el precio de Alquiler")
 if(predict_button):
     total_rent = predictive_model.predict(city, area, rooms, bathrooms, parking_spaces, floors, pets, furniture)
     st.success(f"El resultado de la renta total es: R${round(total_rent, 2)}")
+
+# Seccion de greaficos
+st.subheader("Sección Gráficos de Tendencias de Alquiler")
+# Se toma la data
+data = pd.read_csv("data/houses_to_rent_v2.csv")
+
+# Total de renta por cada una de las ciudades
+average_rent = data.groupby("city")["total (R$)"].mean()
+average_rent.plot(kind="bar", figsize=(10, 6))
+plt.title("Promedio de Renta Total en Ciudad")
+plt.xlabel("Ciudad")
+plt.ylabel("Promedio de Renta")
+st.pyplot()
+
+# Total de renta por la cantidad de cuartos
+rooms_average = data.groupby("rooms")["total (R$)"].mean()
+rooms_average.plot(kind="bar", figsize=(10, 6))
+plt.title("Promedio de Total de Renta por Cuartos")
+plt.xlabel("Cuartos")
+plt.ylabel("Promedio de Renta")
+st.pyplot()
+
+# Total de area por la cantidad de cuartos
+rooms_area_average = data.groupby("rooms")["area"].mean()
+rooms_area_average.plot(kind="bar", figsize=(10, 6))
+plt.title("Promedio de Total de Area por Cuartos")
+plt.xlabel("Cuartos")
+plt.ylabel("Promedio de Area")
+st.pyplot()
